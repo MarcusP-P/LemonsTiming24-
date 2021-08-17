d8c08a4224355c137b22178ade78822ede8dc09d
Marcus Pallinger <git@marcus.pallinger.org>
2021-08-17 20:17:40 +1000

    Add configuration stuff

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using LemonsTiming24.Server.Infrastructure;
+using LemonsTiming24.Server.Infrastructure.BackgroundProcessing;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new() { Title = "LemonsTiming24.Server", Version = "v1" }));
+
+builder.Services.Configure<TimingConfiguration>(builder.Configuration.GetSection("Timing"));
 
 var app = builder.Build();
 
