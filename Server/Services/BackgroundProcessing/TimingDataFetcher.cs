6ce6e067295ebe830cc5fc4087cfd51dfa30ed6e
Marcus Pallinger <git@marcus.pallinger.org>
2022-07-08 17:45:12 +1000

    Log events.

--- Server/Services/BackgroundProcessing/TimingDataFetcher.cs
+++ Server/Services/BackgroundProcessing/TimingDataFetcher.cs
@@ -19,6 +19,14 @@ public class TimingDataFetcher : ITimingDataFetcher, IDisposable
         this.timingConfiguration = timingConfiguration;
     }
 
+    public class ConnectionStatus
+    {
+        public string EventName { get; set; } = "";
+        public string? Reason { get; set; }
+        public int? AttemptNumber { get; set; }
+        public TimeSpan? Duration { get; set; }
+    }
+
     public async Task DoWork(CancellationToken cancellationToken)
     {
         try
@@ -90,19 +98,36 @@ public class TimingDataFetcher : ITimingDataFetcher, IDisposable
         await File.WriteAllTextAsync(path, document);
     }
 
+    private void LogEvent(string eventName, string? reason = null, int? attemptNumber = null, DateTime? eventStart = null, DateTime? eventEnd = null)
+    {
+        var output = new ConnectionStatus
+        {
+            EventName = eventName,
+            Reason = reason,
+            AttemptNumber = attemptNumber,
+            Duration = (eventStart is not null && eventEnd is not null) ? eventEnd - eventStart : null,
+        };
+        var document = JsonSerializer.Serialize(output);
+        _ = this.WriteJsonDocument("aaaInternalDiagnosticsEvent", document);
+    }
+
     private void Client_OnDisconnected(object? sender, string e)
     {
-        System.Diagnostics.Debug.Print($"##### Disconnect after {DateTime.Now - this.clientStartTime:c}: {e}");
+        var now = DateTime.Now;
+        System.Diagnostics.Debug.Print($"##### Disconnect after {now - this.clientStartTime:c} with reason: {e}");
+        this.LogEvent("Disconnect", eventStart: this.clientStartTime, eventEnd: now);
     }
 
     private void Client_OnError(object? sender, string e)
     {
-        System.Diagnostics.Debug.Print($"##### Error");
+        System.Diagnostics.Debug.Print($"##### Error: {e}");
+        this.LogEvent("Error", reason: e);
     }
 
     private void Client_OnReconnected(object? sender, int e)
     {
         System.Diagnostics.Debug.Print($"##### Re Connected");
+        this.LogEvent("Reconnected", attemptNumber: e);
     }
 
     private async void Client_OnConnected(object? sender, EventArgs e)
@@ -112,11 +137,13 @@ public class TimingDataFetcher : ITimingDataFetcher, IDisposable
         await this.client!.EmitAsync("stints:join");
         await this.client!.EmitAsync("laps:join");
         await this.client!.EmitAsync("race_control:join");
+        this.LogEvent("Connected");
     }
 
     private void Client_OnReconnectAttempt(object? sender, int e)
     {
-        System.Diagnostics.Debug.Print($"###### Reconnect Attempt {DateTime.Now:f} #######");
+        System.Diagnostics.Debug.Print($"###### Reconnect Attempt number {e} {DateTime.Now:f} #######");
+        this.LogEvent("Reconnection attempt", attemptNumber: e);
     }
 
     // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
