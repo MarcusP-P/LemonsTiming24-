bee6f42c546664a2381f1a389423a2cfe15194b0
Marcus Helvig <git@marcus.helvig.net>
2023-06-03 18:06:25 +1000

    Switch to new artifacts build system

--- clean.bat
+++ clean.bat
@@ -6,14 +6,5 @@ rd /s /q .vs
 echo Removing packages
 rd /s /q packages
 
-echo Cleaning Client
-rd /s /q Client\bin
-rd /s /q Client\obj
-
-echo Cleaning Server
-rd /s /q Server\bin
-rd /s /q Server\obj
-
-echo Cleaning Shared
-rd /s /q Shared\bin
-rd /s /q Shared\obj
+echo Cleaning Artifacts
+rd /s /q artifacts
